99a1ed838f288e9fc3014c5a482191a49ae7565c
prakashapurv <1986.apurv@gmail.com>
2018-10-05 12:38:33 +0530

    second Update

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,4 @@ node_name                "prakashapurv"
 client_key               "#{current_dir}/prakashapurv.pem"
 chef_server_url          "https://api.chef.io/organizations/prakashapurv"
 cookbook_path            ["#{current_dir}/../cookbooks"]
+knife[:editor]="/usr/bin/vim"
